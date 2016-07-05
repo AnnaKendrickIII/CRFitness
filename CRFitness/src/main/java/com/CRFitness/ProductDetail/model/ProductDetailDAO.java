8e1f04d49bd261f5143c677e1404a6b41c261779
林靖凱 <@EEIT>
2016-07-05 16:36:08 +0800

    修改:ProductDetailDAO 及 interface 	ProductDetailVO 	OrderVO 	ProductDetail映射檔 	Products.jsp
    新增:products2.css
    	products2.js
    	
    整合:ProductDetailService
    	ProductDetailController

--- CRFitness/src/main/java/com/CRFitness/ProductDetail/model/ProductDetailDAO.java
+++ CRFitness/src/main/java/com/CRFitness/ProductDetail/model/ProductDetailDAO.java
@@ -11,17 +11,19 @@ import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
+
 import com.CRFitness.PersonalJournal.model.PersonalJournalVO;
 
 @Repository("productDetailDAO")
-@Transactional(transactionManager="transactionManager")
+@Transactional(transactionManager = "transactionManager")
 public class ProductDetailDAO implements ProductDetailDAO_interface {
 
 	private static final String GET_ALL_STMT = "from ProductDetailVO ";
+	private static final String GET_PRODUCTDETAILID = "from ProductDetailVO where product_Name=:product_Name and size=:size and color=:color";
 
 	@Autowired
-	private SessionFactory sessionFactory ;
-	
+	private SessionFactory sessionFactory;
+
 	public ProductDetailDAO() {
 
 	}
@@ -29,7 +31,6 @@ public class ProductDetailDAO implements ProductDetailDAO_interface {
 	public Session getSession() {
 		return sessionFactory.getCurrentSession();
 	}
-
 
 	@Override
 	public boolean insert(ProductDetailVO productDetailVO) {
@@ -53,7 +54,8 @@ public class ProductDetailDAO implements ProductDetailDAO_interface {
 	public boolean delete(String productDetail_Id) {
 		ProductDetailVO productDetailVO = new ProductDetailVO();
 		productDetailVO.setProductDetail_Id(productDetail_Id);
-		ProductDetailVO productDetailVOs = (ProductDetailVO)this.getSession().get(PersonalJournalVO.class,productDetailVO);
+		ProductDetailVO productDetailVOs = (ProductDetailVO) this.getSession()
+				.get(PersonalJournalVO.class, productDetailVO);
 		if (productDetailVOs != null) {
 			this.getSession().delete(productDetailVOs);
 			return true;
@@ -63,30 +65,38 @@ public class ProductDetailDAO implements ProductDetailDAO_interface {
 
 	@Override
 	public ProductDetailVO findByPrimaryKey(String productDetail_Id) {
-		ProductDetailVO productDetailVO=new ProductDetailVO();
+		ProductDetailVO productDetailVO = new ProductDetailVO();
 		productDetailVO.setProductDetail_Id(productDetail_Id);
-		return (ProductDetailVO) this.getSession().get(ProductDetailVO.class, productDetailVO);
+		return (ProductDetailVO) this.getSession().get(ProductDetailVO.class,
+				productDetailVO);
 	}
 
 	@Override
 	public List<ProductDetailVO> getAll() {
 		Query query = this.getSession().createQuery(GET_ALL_STMT);
-		return (List<ProductDetailVO>)query.list();
+		return (List<ProductDetailVO>) query.list();
 	}
-	
-//	public static void main(String[] args) {
-//		
-//		
-//		ApplicationContext context = new ClassPathXmlApplicationContext("test.config.xml");
-//		ProductDetailDAO productDetailDAO = (ProductDetailDAO)context.getBean("productDetailDAO");
-//		
-//		productDetailDAO.sessionFactory.getCurrentSession().beginTransaction();
-//		ProductDetailVO list = productDetailDAO.findByPrimaryKey("prodDetail4001");
-//		 if(list!=null)
-//		 System.out.print(list.getProductDetail_Id()+","+list.getProduct_Name()+","+list.getStock()
-//				 +","+list.getPublishedDay());
-//		 
-//		 productDetailDAO.sessionFactory.getCurrentSession().getTransaction().commit();
-//		 ((ConfigurableApplicationContext)context).close();
-//	}
+
+	@Override
+	public ProductDetailVO getProductDetailId(String product_Name, String size,
+			String color) {
+		Query query = this.getSession().createQuery(GET_PRODUCTDETAILID);
+		query.setParameter("product_Name", product_Name);
+		query.setParameter("size", size);
+		query.setParameter("color", color);
+		return (ProductDetailVO)query.list().get(0);
+	}
+
+	public static void main(String[] args) {
+
+		ApplicationContext context = new ClassPathXmlApplicationContext(
+				"test.config.xml");
+		ProductDetailDAO_interface productDetailDAO = (ProductDetailDAO_interface) context
+				.getBean("productDetailDAO");
+
+		ProductDetailVO result = productDetailDAO.getProductDetailId("男超寬楦慢跑鞋", "US9", "黃");
+		System.out.println(result.getProductDetail_Id());
+
+		((ConfigurableApplicationContext) context).close();
+	}
 }
--- CRFitness/src/main/java/com/CRFitness/ProductDetail/model/ProductDetailDAO_interface.java
+++ CRFitness/src/main/java/com/CRFitness/ProductDetail/model/ProductDetailDAO_interface.java
@@ -9,6 +9,7 @@ public interface ProductDetailDAO_interface {
      public boolean delete(String productDetail_Id);       
      public ProductDetailVO findByPrimaryKey(String productDetail_Id);
      public List<ProductDetailVO> getAll();
+     public ProductDetailVO getProductDetailId(String product_Name, String size, String color);
 	
 	
 }
