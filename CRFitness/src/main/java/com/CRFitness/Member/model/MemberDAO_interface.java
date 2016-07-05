3cbdc3bf53d3b80b20c29fd7de6a9279e443977f
鄭文智 <@III-ITC>
2016-07-05 10:47:08 +0800

    Initial commit

---
+++ CRFitness/src/main/java/com/CRFitness/Member/model/MemberDAO_interface.java
@@ -0,0 +1,12 @@
+package com.CRFitness.Member.model;
+
+import java.util.*;
+
+public interface MemberDAO_interface {
+          public boolean insert(MemberVO memberVO);
+          public boolean update(MemberVO memberVO);
+          public boolean delete(String member_Id);       
+          public MemberVO findByPrimaryKey(String member_Id);
+          public List<MemberVO> select_email (String e_mail);
+          public List<MemberVO> getAll();
+}
---
+++ CRFitness/src/main/java/com/CRFitness/ProductDetail/model/ProductDetailDAO_interface.java
@@ -0,0 +1,14 @@
+package com.CRFitness.ProductDetail.model;
+
+import java.util.List;
+
+public interface ProductDetailDAO_interface {
+	
+	 public boolean insert(ProductDetailVO productDetailVO);
+     public boolean update(ProductDetailVO productDetailVO);
+     public boolean delete(String productDetail_Id);       
+     public ProductDetailVO findByPrimaryKey(String productDetail_Id);
+     public List<ProductDetailVO> getAll();
+	
+	
+}
