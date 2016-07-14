da16a5c076d0ba52f85fd06cfa1071f08c8c3dde
鄭文智 <@III-ITC>
2016-07-14 16:36:02 +0800

    後台：修改"修改的功能"

--- CRFitness/src/main/java/com/CRFitness/ProductDetail/model/ProductDetailService.java
+++ CRFitness/src/main/java/com/CRFitness/ProductDetail/model/ProductDetailService.java
@@ -152,7 +152,11 @@ public class ProductDetailService {
 	}
 
 	// back-end 修改商品至 ProductDetail & Product Table
-	public List<Object> updateProductDetail(String product_Name, Double price,
+	public List<Object> updateProductDetail(
+			String product_Id,
+			String productDetail_Id,
+			String product_Name, 
+			Double price,
 			String category, String size, // 尺寸
 			String color, // 顏色
 			Integer stock, // 庫存量
@@ -164,13 +168,15 @@ public class ProductDetailService {
 			String introduction) {
 		List<Object> list = new ArrayList<Object>();
 		
-		ProductsVO productsVO = new ProductsVO();
+//		ProductsVO productsVO = new ProductsVO();
+		ProductsVO productsVO = productsDAO.findByPrimaryKey(product_Id);	
 		productsVO.setProduct_Name(product_Name);
 		productsVO.setPrice(price);
 		productsVO.setCategory(category);
 		productsVO = productsDAO.update(productsVO);
 		
-		ProductDetailVO productDetailVO = new ProductDetailVO();
+//		ProductDetailVO productDetailVO = new ProductDetailVO();
+		ProductDetailVO productDetailVO = productDetailDAO.findByPrimaryKey(productDetail_Id);	
 		productDetailVO.setProduct_Id(productsVO.getProduct_Id());
 		productDetailVO.setProduct_Name(productsVO.getProduct_Name());
 		productDetailVO.setSize(size);
@@ -179,6 +185,7 @@ public class ProductDetailService {
 		Timestamp ts = new Timestamp(System.currentTimeMillis());
 		productDetailVO.setPublished_Date(ts);
 		
+		System.out.println(productDetailVO.getPublished_Date());
 		try {
 			productDetailVO.setPhoto1(photo1.getBytes());
 		} catch (IOException e) {
