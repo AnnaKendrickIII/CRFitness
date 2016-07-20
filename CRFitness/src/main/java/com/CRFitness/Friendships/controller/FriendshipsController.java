9e84e091a206cd362d16581eff335a222d3d14a3
翁光志 <@III-ITC>
2016-07-20 21:40:29 +0800

    沒事多commit

--- CRFitness/src/main/java/com/CRFitness/Friendships/controller/FriendshipsController.java
+++ CRFitness/src/main/java/com/CRFitness/Friendships/controller/FriendshipsController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.CRFitness.Friendships.model.FriendshipsService;
@@ -27,5 +28,20 @@ public class FriendshipsController {
 	public @ResponseBody List<MemberVO> findFriends(
 			@PathVariable String member_Id) {		
 		return friendshipsService.findFriends(member_Id);
+	}
+	
+	
+	@RequestMapping(method = RequestMethod.POST, value = "/addFriends", produces = MediaType.APPLICATION_JSON)
+	public @ResponseBody boolean addFriends(
+			@RequestParam String member_Id,
+			@RequestParam String friend_Id) {
+		return friendshipsService.addFriend(member_Id, friend_Id);
+	}
+	
+	@RequestMapping(method = RequestMethod.POST, value = "/deleteFriends", produces = MediaType.APPLICATION_JSON)
+	public @ResponseBody boolean deleteFriends(
+			@RequestParam String member_Id,
+			@RequestParam String friend_Id) {
+		return friendshipsService.updateFriend(member_Id, friend_Id, 0);
 	}
 }
