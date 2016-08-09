984c0d388491e62a928855bbf81b994bd8729199
鄭文智 <@III-ITC>
2016-08-09 18:45:37 +0800

    補上被誤刪的關於後台java檔。

--- CRFitness/src/main/java/com/CRFitness/Activitys/controller/ActivitysControllerBE.java
+++ CRFitness/src/main/java/com/CRFitness/Activitys/controller/ActivitysControllerBE.java
@@ -1,22 +1,16 @@
 package com.CRFitness.Activitys.controller;
 
-import java.io.UnsupportedEncodingException;
-import java.util.List;
 
 import javax.annotation.Resource;
-import javax.servlet.http.HttpServletRequest;
-import javax.ws.rs.core.MediaType;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.multipart.MultipartFile;
 
 import com.CRFitness.Activitys.model.ActivitysService;
-import com.CRFitness.Activitys.model.ActivitysVO;
+
 @Controller
 @RequestMapping("/activitysControllerBE")
 public class ActivitysControllerBE {
